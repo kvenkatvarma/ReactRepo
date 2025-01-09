82f6600165bebdadbc0de355bcbed5f8d6a10f35
Venkat varma <kvenkatvarma@gmail.com>
2025-01-09 14:35:53 +0530

    Render List and bind to table

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 
 export default class MainContent extends Component
 {
-    state ={pageTitle:"Customers",customersCount:5};
+    state ={pageTitle:"Customers",customersCount:5,
+        customers:[
+            {id:1,name:"Venkat",phone:"123456",address:{city:"Hyd"}},
+            {id:2,name:"Durga",phone:"787878",address:{city:"Hyd"}},
+            {id:3,name:"Nitya",phone:"567698",address:{city:"Hyd"}},
+            {id:4,name:"ABC",phone:"87876",address:{city:"ABC"}},
+            {id:5,name:"DEF",phone:"909989",address:{city:"Upl"}}       
+         ]
+    };
     render()
     {
         return (
@@ -11,7 +19,31 @@ export default class MainContent extends Component
             <span className="badge badge-secondary m-2">{this.state.customersCount}</span>
             <button className="btn btn-info" onClick={this.onRefreshClick}>Refresh</button>
             </h4>
-            
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Customer Name</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.customers.map((cust)=>{
+                         return(
+                            <tr key={cust.id}>
+                                <td>{cust.id}</td>
+                                <td>{cust.name}</td>
+                                <td>{cust.phone}</td>
+                                <td>{cust.address.city}</td>
+                            </tr>
+                         )
+                        })
+                    }
+                    
+                </tbody>
+            </table>
         </div>
         )
     }

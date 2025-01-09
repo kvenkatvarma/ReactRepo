e118943f1363d67db79a91926f2e2548a75da4f8
Venkat varma <kvenkatvarma@gmail.com>
2025-01-09 13:08:29 +0530

    Replaced Class with ClassName to avoid ambiguity with Class Keyword for creating vlass

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ class NavBar extends Component{
 render(){
     return (
         <React.Fragment>
-          <nav class="navbar navbar-expand-lg bg-body-tertiary"> 
-  <div class="container-fluid navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
-    <a class="navbar-brand" href="#">My App</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+          <nav className="navbar navbar-expand-lg bg-body-tertiary"> 
+  <div className="container-fluid navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+    <a className="navbar-brand" href="/#">My App</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
       
-        <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
+        <li className="nav-item">
+          <a className="nav-link" href="/#">Home</a>
         </li>    
        
       </ul>     

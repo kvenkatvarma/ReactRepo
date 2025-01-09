75a569ad94250f1fcd3c834d88687b7fc7b72c6b
Venkat varma <kvenkatvarma@gmail.com>
2025-01-09 12:50:01 +0530

    Created Component, Fragment and Export default

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import  NavBar  from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   <button className='btn btn-danger'>Hello React</button>
+  <React.StrictMode> 
+   <NavBar/>
   </React.StrictMode>
 );

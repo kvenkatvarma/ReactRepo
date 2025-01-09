cf94df9ff4419833d553a58ea68d31aca6d6522d
Venkat varma <kvenkatvarma@gmail.com>
2025-01-09 13:04:19 +0530

    Adding Bootstrap Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,23 @@ class NavBar extends Component{
 render(){
     return (
         <React.Fragment>
-            <h1 class="highlight">Hello</h1>
-            <p>This is a paragraph</p>
+          <nav class="navbar navbar-expand-lg bg-body-tertiary"> 
+  <div class="container-fluid navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
+    <a class="navbar-brand" href="#">My App</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>    
+       
+      </ul>     
+    </div>
+  </div>
+</nav>
         </React.Fragment>
     );
 }

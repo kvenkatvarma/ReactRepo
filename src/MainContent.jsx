8f19eba9f746cf02846d0b63ef1980ebee0438c3
Venkat varma <kvenkatvarma@gmail.com>
2025-01-09 13:39:23 +0530

    Render Expressions

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 
 export default class MainContent extends Component
 {
+    state ={appTitle:"Employees"};
     render()
     {
-        return <div>Hello From Main Content</div>
+        return (
+        <div>
+            <h4 className="border-bottom m-1 p-1">{this.state.appTitle}</h4>
+        </div>
+        )
     }
 }

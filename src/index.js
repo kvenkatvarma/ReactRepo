0e20849ddb07a8d439c475e9b870d9fef290a4af
Venkat varma <kvenkatvarma@gmail.com>
2025-01-09 13:30:12 +0530

    Nested Components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import  NavBar  from './App';
 import "./index.css";
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
-   <NavBar/>
+    <App></App>
   </React.StrictMode>
 );

37a4eb540b53a8fcd713407857f896249e3f97c6
Venkat varma <kvenkatvarma@gmail.com>
2025-01-09 16:45:22 +0530

    Props VS State

--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -19,7 +19,7 @@ export default class ShoppingCart extends Component{
             <div className="row">
                 {
                     this.state.products.map((prod)=>{
-                     return <Product key={prod.id} id={prod.id} productName={prod.productName} price={prod.price}/>
+                     return <Product key={prod.id} product={prod}/>
                     })
                 }
             </div>

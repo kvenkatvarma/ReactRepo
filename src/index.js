7ff52d57dde0c55990be64c8120fb1d665e6773e
Venkat varma <kvenkatvarma@gmail.com>
2025-01-09 12:17:02 +0530

    Load Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Note the change here
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <div>Hello React</div>
+   <button className='btn btn-danger'>Hello React</button>
   </React.StrictMode>
 );

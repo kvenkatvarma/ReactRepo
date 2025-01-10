f6a6201a984ac4e67787c16d36721078b9adf3f3
Venkat varma <kvenkatvarma@gmail.com>
2025-01-10 10:43:19 +0530

    Basic Route using react-router-dom version 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React,{ Component } from "react"
 import NavBar from "./NavBar"
 import Login from "./Login"
-
+import ShoppingCart from "./ShoppingCart"
+import Dashboard from "./Dashboard"
+import { Route, Switch,BrowserRouter } from "react-router-dom"; 
+import CustomersList from "./CustomersList"
 export default class App extends Component{
    render(){
     return(
-        <React.Fragment>
-            <NavBar/>
-            <Login/>
-        </React.Fragment>
       
+      <BrowserRouter>
+      <NavBar />
+      <Switch>  {/* Use Switch to wrap your Route components */}
+        <Route path="/" exact component={Login} />  {/* Use the `component` prop in React Router v5 */}
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route path="/customers" exact component={CustomersList} />
+        <Route path="/cart" exact component={ShoppingCart} />       
+      </Switch>
+    </BrowserRouter>
     )
    }
 }

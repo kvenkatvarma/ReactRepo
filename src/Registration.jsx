877ab88922bfbe53007b2d65ed8c6bef29e928ea
Venkat varma <kvenkatvarma@gmail.com>
2025-01-20 19:01:05 +0530

    validate

--- src/Registration.jsx
+++ src/Registration.jsx
@@ -3,7 +3,14 @@ class Register extends Component{
     constructor(props){
         super(props);
         this.state={
-            email:"",password:"",fullName:"",dateOfBirth:""
+            email:"",password:"",fullName:"",dateOfBirth:"",
+            controls:["email","password","fullName","dateOfBirth"],
+            errors:{
+                email:[],
+                password:[],
+                fullName:[],
+                dateOfBirth:[],
+            },
         };
     }
     render(){
@@ -15,7 +22,8 @@ class Register extends Component{
                       <label className="col-lg-4 col-form-label" htmlFor="email">Email</label>
                       <div className="col-lg-8">
                         <input type="text" className="form-control" id="email" value={this.state.email} onChange={(event)=>{
-                            this.setState({email:event.target.value})
+                            this.setState({email:event.target.value},this.validate);
+                           
                         }}/>
                       </div>
                     </div>
@@ -24,7 +32,8 @@ class Register extends Component{
                       <label className="col-lg-4 col-form-label" htmlFor="password">Password</label>
                       <div className="col-lg-8">
                         <input type="password" className="form-control" id="password" value={this.state.password} onChange={(event)=>{
-                            this.setState({password:event.target.value})
+                            this.setState({password:event.target.value},this.validate);
+                           
                         }}/>
                       </div>
                     </div>
@@ -33,7 +42,8 @@ class Register extends Component{
                       <label className="col-lg-4 col-form-label" htmlFor="fullname">Full Name</label>
                       <div className="col-lg-8">
                         <input type="text" className="form-control" id="fullname" value={this.state.fullName} onChange={(event)=>{
-                            this.setState({fullName:event.target.value})
+                            this.setState({fullName:event.target.value},this.validate);
+                          
                         }}/>
                       </div>
                     </div>
@@ -42,7 +52,8 @@ class Register extends Component{
                       <label className="col-lg-4 col-form-label" htmlFor="dateOfBirth">Date of Birth</label>
                       <div className="col-lg-8">
                         <input type="date" className="form-control" id="dateOfBirth" value={this.state.dateOfBirth} onChange={(event)=>{
-                            this.setState({dateOfBirth:event.target.value})
+                            this.setState({dateOfBirth:event.target.value},this.validate);
+                           
                         }}/>
                       </div>
                     </div>
@@ -52,12 +63,39 @@ class Register extends Component{
                           <div className="text-right">
                             <button className="btn btn-success m-2" onClick={this.onRegisterClick}>Register</button>
                           </div>
+                          <ul className="text-danger">
+                            {Object.keys(this.state.errors).map((control)=>{
+                                     return this.state.errors[control].map((err)=>{
+                                       return <li key={err}>{err}</li>
+                                     })
+                            })}
+                          </ul>
                        </div>
                    </div>
                 </div>
             </div>
         );
     }
+    validate=()=>{
+        let errors = {};
+        this.state.controls.forEach((control)=>{       
+            errors[control]=[];
+           switch(control)
+           {
+            case "email":
+              if(!this.state[control])
+                {
+                 errors[control].push("Email cannot be blank");
+                } 
+            break;
+
+            default:
+
+             break;
+           }
+        });
+        this.setState({errors})
+    };
 }
 
 export default Register;
